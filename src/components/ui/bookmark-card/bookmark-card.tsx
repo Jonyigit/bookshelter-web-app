e268a196d3a289daa6a558cac767cf380f503375
Jonyigit <jonyigitavazbekov@gmail.com>
2025-11-02 13:23:41 +0500

    fix bug

--- src/components/ui/bookmark-card/bookmark-card.tsx
+++ src/components/ui/bookmark-card/bookmark-card.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { FiDelete, FiBookOpen } from "react-icons/fi";
+import { Tooltip } from "react-tooltip";
 
 const Wrapper = styled.div`
     background: var(--bg);
@@ -32,16 +33,38 @@ const Button = styled.button`
     font-size: 20px;
 `;
 
-function BookmarkCard({ item }: any) {
-    console.log(item);
+const Title = styled.h4`
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    cursor: default;
+    font-size: 16px;
+    font-weight: 400;
+    color: var(--text-color);
+`;
 
+const Desc = styled.span`
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    cursor: default;
+    font-size: 13px;
+    font-weight: 400;
+    color: var(--color);
+`;
+
+function BookmarkCard({ item }: any) {
     return (
         <Wrapper>
             <Content>
-                <h4 style={{ fontSize: "16px", fontWeight: "400", color: "var(--text-color)" }}>{item.title}</h4>
-                <span style={{ fontSize: "13px", fontWeight: "500", color: "var(--color)" }}>
-                    {item.authors.join(",")}
-                </span>
+                <Title>{item.title}</Title>
+                <Desc>{item.authors.join(",")}</Desc>
             </Content>
             <Controls>
                 <Button>
